4f772dd5af1776c8049a6f3ba9ab33a8e96c2947
jehadhamid <jehadadhame@gmail.com>
2026-05-22 20:19:38 +0300

    add feed.e-mall.site to allowedHosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+
   plugins: [react()],
   server: {
+    allowedHosts: ["e-mall.site", "feed.e-mall.site"],
     host: "127.0.0.1",
     port: 5173,
   },
